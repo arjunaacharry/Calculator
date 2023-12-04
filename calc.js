9b9d5410ac850536902be51908bbac8faeb1d4ef
Arjun A Acharry <arjunaacharry007@gmail.com>
2023-12-04 20:37:07 +0530

    Code update

--- calc.js
+++ calc.js
@@ -1,14 +1,19 @@
 var screen = document.getElementById('screen');
+var resultCalculated = false;
 
-function btnClick(value){
+function btnClick(value) {
+    if (resultCalculated) {
+        clearScreen();
+        resultCalculated = false;
+    }
     screen.value += value;
 }
-
 function clearScreen(){
     screen.value = "";
 }
 
-function findResult(){
-    var result = eval(screen.value)
+function findResult() {
+    var result = eval(screen.value);
     screen.value = result;
+    resultCalculated = true;
 }
